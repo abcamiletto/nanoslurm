3ef5ba6b0086cbe7ebd0cc18f0ee150ee9b1afb9
Andrea Boscolo Camiletto <53488500+abcamiletto@users.noreply.github.com>
2025-09-12 13:54:14 +0200

    factor out defaults into reusable module??

--- src/nanoslurm/__init__.py
+++ src/nanoslurm/__init__.py
@@ -5,6 +5,14 @@ import sys
 if not sys.platform.startswith("linux"):
     raise OSError("nanoslurm is only supported on Linux")
 
+from .defaults import DEFAULTS, KEY_TYPES, load_defaults, save_defaults
 from .nanoslurm import Job, submit
 
-__all__ = ["Job", "submit"]
+__all__ = [
+    "Job",
+    "submit",
+    "DEFAULTS",
+    "KEY_TYPES",
+    "load_defaults",
+    "save_defaults",
+]
